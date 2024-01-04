57612d288dca9cb3291aa491167f8ea14c3cdff1
fxckxff <127879194+fxckxff@users.noreply.github.com>
2024-01-04 20:31:01 +0100

    bot_project
    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import config
+import telebot
+
+bot = telebot.TeleBot(config.token)
+
+# @bot.message_handler(content_types=["text"])
+# def repeat_all_messages(message): # Название функции не играет никакой роли
+#     bot.send_message(message.chat.id, message.text)
+
+@bot.message_handler(commands=['start','help'])
+def send_welcome(message):
+    bot.reply_to(message, 'howdy, how are you?')
+    print(message.chat.id)
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+if __name__ == '__main__':
+     bot.infinity_polling()
