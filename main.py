8cecf6429b5a7e3e5713ea04339c49bce9a8ecf4
fxckxff <127879194+fxckxff@users.noreply.github.com>
2024-01-06 14:59:37 +0000

    add a function which return price of btc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import config
 import telebot
+import requests
 
 bot = telebot.TeleBot(config.token)
 
@@ -12,9 +13,20 @@ def send_welcome(message):
     bot.reply_to(message, 'Hallo')
     print(message.chat.id)
 
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+# @bot.message_handler(func=lambda m: True)
+# def echo_all(message):
+#     bot.reply_to(message, message.text)
 
+def get_price():
+    url = 'https://api.coinbase.com/v2/exchange-rates?currency=BTC'
+    response = requests.get(url)
+    data = response.json()
+    return data['data']['rates']['USD']+'$'
+
+@bot.message_handler(commands=['btc'])
+def send_price(message):
+    bot.send_message(message.chat.id, get_price())
+
+print('bot arbeitet')
 if __name__ == '__main__':
      bot.infinity_polling()
