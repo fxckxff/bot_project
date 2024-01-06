c0f9603b0a0816b63478d59c18d1dbb92cf41c4d
fxckxff <127879194+fxckxff@users.noreply.github.com>
2024-01-06 20:02:32 +0000

    start work with binance API. Add get_rsi function

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import config
 import telebot
 import requests
+import ccxt
 
 bot = telebot.TeleBot(config.token)
+binance_api_key = config.api_key
+binance_secret_key = config.secret_key
 
-# @bot.message_handler(content_types=["text"])
-# def repeat_all_messages(message): # Название функции не играет никакой роли
-#     bot.send_message(message.chat.id, message.text)
+binance_client = ccxt.binance({
+    'apiKey': binance_api_key,
+    'secret': binance_secret_key,
+})
 
-@bot.message_handler(commands=['start','help'])
-def send_welcome(message):
-    bot.reply_to(message, 'Hallo')
-    print(message.chat.id)
+# Получение данных о RSI для выбранной пары торгов
+def get_rsi(symbol='BTC/USDT', timeframe='1h', period=14):
+    ohlcv = binance_client.fetch_ohlcv(symbol, timeframe)
+    closes = [tick[4] for tick in ohlcv]
+    rsi = binance_client.rsi(closes, period)
+    return rsi
 
-# @bot.message_handler(func=lambda m: True)
-# def echo_all(message):
-#     bot.reply_to(message, message.text)
+# Функция для определения сигнала на вход в сделку на основе RSI
+def trading_strategy(symbol='BTC/USDT', timeframe='1h', rsi_threshold=60):
+    rsi = get_rsi(symbol, timeframe)
+    print(f'Current RSI for {symbol} ({timeframe}): {rsi}')
+
+    # Если RSI больше или равно пороговому значению, возвращаем True
+    return rsi >= rsi_threshold
+
+
+# Пример использования
+if trading_strategy():
+    print("Сигнал на вход в сделку!")
+else:
+    print("Нет сигнала на вход в сделку.")
+
 
 def get_price():
     url = 'https://api.coinbase.com/v2/exchange-rates?currency=BTC'
